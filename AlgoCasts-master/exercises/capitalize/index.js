37d56a5764a32fb51c6eacef2931c092d23207e3
AndrewSimons3 <simdrew07@gmail.com>
2022-03-02 07:31:45 -0600

    capitalize

--- AlgoCasts-master/exercises/capitalize/index.js
+++ AlgoCasts-master/exercises/capitalize/index.js
@@ -16,12 +16,12 @@
 //     words.push(word[0].toUpperCase() + word.slice(1));
 //   }
 
-//   return words.join(' ');
+//  return words.join(' ');
 // }
 
 function capitalize(str) {
   let result = str[0].toUpperCase();
-  
+
   for (let i = 1; i < str.length; i++) {
     if (str[i - 1] === ' ') {
       result += str[i].toUpperCase();
@@ -29,7 +29,7 @@ function capitalize(str) {
       result += str[i];
     }
   }
-
+  
   return result;
 }
 
